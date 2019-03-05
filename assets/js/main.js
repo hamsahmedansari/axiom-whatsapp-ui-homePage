86cc736a218dfe262a76adedaca2ade54a896e21
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-05 16:53:52 +0500

    :rocket: Chat Tab ,Status Tab and Call Tab Ui is done with fade in animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,60 @@ document.addEventListener("scroll", () => {
       .classList.remove("section-bottom");
   }
 });
+
+function statusClick() {
+  document.getElementsByClassName("chats-tab")[0].classList.remove("active");
+  document.getElementsByClassName("calls-tab")[0].classList.remove("active");
+  document.getElementsByClassName("status-tab")[0].classList.add("active");
+
+  //
+
+  document.getElementsByClassName("chats-page")[0].classList.add("d-none");
+  document.getElementsByClassName("calls-page")[0].classList.add("d-none");
+  document.getElementsByClassName("story-page")[0].classList.remove("d-none");
+  document.getElementsByClassName("story-page")[0].classList.add("opacity-0");
+  setTimeout(() => {
+    document
+      .getElementsByClassName("story-page")[0]
+      .classList.remove("opacity-0");
+    document.getElementsByClassName("story-page")[0].classList.add("opacity-1");
+  }, 300);
+}
+
+function chatsClick() {
+  document.getElementsByClassName("status-tab")[0].classList.remove("active");
+  document.getElementsByClassName("calls-tab")[0].classList.remove("active");
+  document.getElementsByClassName("chats-tab")[0].classList.add("active");
+
+  //
+
+  document.getElementsByClassName("story-page")[0].classList.add("d-none");
+  document.getElementsByClassName("calls-page")[0].classList.add("d-none");
+  document.getElementsByClassName("chats-page")[0].classList.remove("d-none");
+  document.getElementsByClassName("chats-page")[0].classList.add("opacity-0");
+  setTimeout(() => {
+    document
+      .getElementsByClassName("chats-page")[0]
+      .classList.remove("opacity-0");
+    document.getElementsByClassName("chats-page")[0].classList.add("opacity-1");
+  }, 300);
+}
+
+function callsClick() {
+  document.getElementsByClassName("status-tab")[0].classList.remove("active");
+  document.getElementsByClassName("chats-tab")[0].classList.remove("active");
+  document.getElementsByClassName("calls-tab")[0].classList.add("active");
+
+  //
+
+  document.getElementsByClassName("story-page")[0].classList.add("d-none");
+  document.getElementsByClassName("chats-page")[0].classList.add("d-none");
+  document.getElementsByClassName("calls-page")[0].classList.remove("d-none");
+  document.getElementsByClassName("calls-page")[0].classList.add("opacity-0");
+  setTimeout(() => {
+    document
+      .getElementsByClassName("calls-page")[0]
+      .classList.remove("opacity-0");
+    document.getElementsByClassName("calls-page")[0].classList.add("opacity-1");
+  }, 300);
+}
